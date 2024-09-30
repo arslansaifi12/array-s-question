4ebe78b40737367fade17a3ca2256e917f2b7349
arslansaifi12 <arslangzb88@gmail.com>
2024-09-30 07:40:59 -0700

    Update array-q.js
    add some question

--- array-q.js
+++ array-q.js
@@ -130,7 +130,128 @@ let arslan=[10,20,]
 
 for (let maal of arslan) {
     console.log(maal);
-    
-    
 }
+
+
+
+
+
+
+//  // repeating element in array
+// // question 30,40 kaise nikale 
+// let repeatingelement=(arr)=>{
+
+//     let notunique=[]
+
+    
+// for(let i=0; i<arr.length; i++){
+//     let isunique=true;
+
+//     for(let j=i+1; j<arr.length; j++){
+//         if (!(arr[i]==arr[j])) {
+
+//             isunique=false       
+//         }
+//         if (isunique) {
+
+//             notunique[notunique.length]=arr[i]
+
+//             break;
+
+//         }
+
+//       }
+
+//     }
+//     return notunique;
+// }
+// let rv =repeatingelement([10,10,20,20,30,40, 50,50,60,60])
+// console.log(rv);
+
+
+
+
+
+//remove duplicate element in array dry run 
+
+// let arr=[10,10,20,20,30,40, 50,50,60,60]
+
+// let notunique=[];
+
+// for(let i=0; i<arr.length; i++){
+//     let isunique=true;
+//     for(let j=i+1; j<arr.length; j++){
+//         if ((arr[i]==arr[j])) {
+//            isunique=false
+            
+           
+//         }
+//        if (isunique) {
+//         notunique[notunique.length]=arr[i]
+//         break;
+//        }
+
+//       }
+
+//       }
+
+//       console.log(notunique);
+      
+      
+// // questio return kaise kare dono ko
+// let symmetricpairq=(arr)=>{
+
+// let symmetricpair=[];
+// let symmetricpair1=[];
+
+// for(let i=0; i<arr.length; i++){
+//     for(let j=i+1; j<arr.length; j++){
+//         if (arr[i][0]==arr[j][1] && arr[i][1]==arr[j][0]) {
+//             symmetricpair=(arr[i])
+//             symmetricpair1=(arr[j])
+            
+//            console.log(symmetricpair,symmetricpair1);
+           
+//         }
+//     }
+// }
+
+
+// }
+// symmetricpairq([[10,20],[20,10],[30,40],[40,30],[50,60],[10,20]])
+
+
+
+
+// let sortedarr=(arr)=>{
+    
+// for(let i=0; i<arr.length; i++){
+//     for(let j=i+1; j<arr.length; j++){
+//         if (arr[i]>arr[j]) {
+//             let temp=arr[i]
+//             arr[i]=arr[j]
+//             arr[j]=temp
+          
+//         }
+//     }
+// }
+// return arr
+
+// }
+
+//  let rv=sortedarr([40,10,30,20])
+
+// console.log(rv);
+
+
+
+
+i=10,20,41
+j=5
+let temp=j
+
+i=""+j+i
+console.log(i);
+
+
 
